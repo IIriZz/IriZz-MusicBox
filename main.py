22386855735df2a533c8b678223b70958275a097
IIriZz <sid000rn@gmail.com>
2022-04-04 10:44:45 +0300

    commit #2

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+# main.py
+
+import os
 
 from discord.ext import commands
 import music
+
+
+with open('BAD_WORDS.txt', 'r', encoding='UTF-8') as file:
+    bad_words = file.read().split(', ')
 
 
 cogs = [music]
@@ -10,10 +17,21 @@ client = commands.Bot(command_prefix='$')
 client.remove_command('help')
 
 
+@client.event
+async def on_message(ctx):
+    await client.process_commands(ctx)
+
+    msg = ctx.content.lower()
+
+    if msg in bad_words:
+        await ctx.delete()
+        await ctx.send(f'**{ctx.message.author}**, нахуя материшья? Охуел вообще, уебан жирный')
+
+
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
 
 if __name__ == '__main__':
-    token = open('TOKEN.txt', 'r').readline()
+    token = os.getenv('TOKEN')
     client.run(token)
--- music.py
+++ music.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 # music.py
 
+from random import choice
+
 import discord
 import youtube_dl
 from discord.ext import commands
+
+
+def generate_name():
+    with open('WORDS.txt', 'r', encoding='UTF-8') as file:
+        return file.read().split('\n')
+
+
+def generate_names():
+    with open('WORDS2.txt', 'r', encoding='UTF-8') as file:
+        return file.read().split('\n')
 
 
 class Music(commands.Cog):
@@ -12,8 +24,9 @@ class Music(commands.Cog):
 
     @commands.command()
     async def join(self, ctx):
+        await ctx.send(f'Вы все {choice(generate_names())}!')
         if ctx.author.voice is None:
-            await ctx.send('Ты меня не слушаешь 👂')
+            await ctx.send('Пусть меня хоть кто-нибудь послушает 👂')
         voice_channel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await voice_channel.connect()
@@ -22,26 +35,35 @@ class Music(commands.Cog):
 
     @commands.command()
     async def disconnect(self, ctx):
-        await ctx.send('⚡ Съёбываю ⚡')
+        await ctx.send('⚡ **Активно съёбываю** ⚡')
         await ctx.voice_client.disconnect()
 
     @commands.command()
     async def pause(self, ctx):
         if ctx.voice_client.is_playing():
+            await ctx.send(f'**{ctx.message.author}** поставил на паузу ⏸')
             await ctx.voice_client.pause()
         else:
-            await ctx.send(f'{ctx.message.author}, ты уши по утрам моешь? Музыка не играет вообще-то ⏸')
+            await ctx.send(f'**{ctx.message.author}**, ты уши по утрам моешь? Музыка не играет вообще-то ⏸')
 
     @commands.command()
     async def resume(self, ctx):
         if ctx.voice_client.is_paused():
+            await ctx.send(f'**{ctx.message.author}** продолжил музыку ▶')
             await ctx.voice_client.resume()
         else:
-            await ctx.send(f'{ctx.message.author}, ты совсем глухой? Музыка тут давно ебашит 🎶')
+            await ctx.send(f'**{ctx.message.author}**, ты совсем глухой? Музыка тут давно ебашит 🎶')
 
     @commands.command()
     async def play(self, ctx, url):
         ctx.voice_client.stop()
+        print(ctx.message.author)
+        if ctx.message.author == 'IriZz#7910':
+            await ctx.send(f'Могучий\nПрекрасный\nУжасающий\nСтрастный\nИзумительный\nВсепобеждающий\nНеустанный\n'
+                           f'Решительный\nЧарующий\nТаинственный\nЧувственный\nБесстрашный\nНепобедимый\n'
+                           f'**{ctx.message.author}** решил включить прекрасную музыку')
+        else:
+            await ctx.send(f'Какой-то {choice(generate_name())} по имени **{ctx.message.author}** запустил это')
 
         FFMPEG_OPTIONS = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
@@ -54,27 +76,27 @@ class Music(commands.Cog):
 
         vc = ctx.voice_client
 
+
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
             url2 = info['formats'][0]['url']
             source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
             vc.play(source)
-        await ctx.send(f'Какой-то гондон, по имени {ctx.message.author}, запустил это')
 
 
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def help(self, ctx):
         emb = discord.Embed(
-            title='Помощь для низших форм жизни',
-            colour=discord.Colour.gold()
+            title='**Помощь для низших форм жизни**',
+            colour=discord.Colour.purple()
         )
 
-        emb.add_field(name='$join', value='Вступить в голосовой чат 👂', inline=False)
-        emb.add_field(name='$play {url}', value='Сыграть музыку {ссылка на музыку} 🎶', inline=False)
-        emb.add_field(name='$pause', value='Поставить музыку на паузу ⏸', inline=False)
-        emb.add_field(name='$resume', value='Продолжить играть музыку ▶', inline=False)
-        emb.add_field(name='$disconnect', value='Выйти из голосового чата ⚡', inline=False)
+        emb.add_field(name='**$join**', value='Вступить в голосовой чат 🎤', inline=False)
+        emb.add_field(name='**$play {url}**', value='Сыграть музыку {ссылка на музыку} 🎶', inline=False)
+        emb.add_field(name='**$pause**', value='Поставить музыку на паузу ⏸', inline=False)
+        emb.add_field(name='**$resume**', value='Продолжить играть музыку ▶', inline=False)
+        emb.add_field(name='**$disconnect**', value='Выйти из голосового чата ⚡', inline=False)
 
         await ctx.send(embed=emb)
 
